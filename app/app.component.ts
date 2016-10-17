874796709046069f8616b196f74d9f661c3404a4
Alison Moura <alison.oghino@gmail.com>
2016-10-17 20:14:06 -0300

    Adding ngModel in input hero's name

--- app/app.component.ts
+++ app/app.component.ts
@@ -7,7 +7,17 @@ export class Hero {
 
 @Component({
   selector: 'my-app',
-  template: '<h1>{{title}}</h1><h2>{{hero.name}} details</h2>'
+  template: `
+  <h1>{{title}}</h1>
+  <h2>{{hero.name}} details</h2>
+  <div>
+    <h2><label>id: </label>{{hero.id}}</h2>
+  </div>
+  <div>
+    <label>name: </label>
+    <input type="text" [(ngModel)]="hero.name" placeholder="name">
+  </div>
+  `
 })
 export class AppComponent {
   title = 'Tour of Hero';
