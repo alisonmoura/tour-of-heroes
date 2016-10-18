005ab580d4ba516efc6989c56f05822d05b8de42
Alison Moura <alison.oghino@gmail.com>
2016-10-17 23:23:18 -0300

    Creating HeroDetailComponent and organizing files

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,11 +1,12 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule }   from '@angular/forms';
-import { AppComponent }   from './app.component';
+import { AppComponent }   from './components/app.component';
+import { HeroDetailComponent } from './components/hero-detail.component'
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule],
-  declarations: [ AppComponent ],
+  declarations: [ AppComponent, HeroDetailComponent ],
   bootstrap:    [ AppComponent ]
 })
 
