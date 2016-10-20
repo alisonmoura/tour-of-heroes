5fa06f277187e87e0d46bbf323f17b54798216da
Alison Moura <alison.oghino@gmail.com>
2016-10-20 12:28:28 -0300

    Creating HeroService

--- app/services/hero.service.ts
+++ app/services/hero.service.ts
@@ -1,6 +1,11 @@
 import { Injectable } from '@angular/core';
 
+import { Hero } from './../models/hero';
+import { HEROES } from './../mocks/mock-heroes';
+
 @Injectable()
 export class HeroService{
-    
+    getHeroes() : Hero[]{
+        return HEROES;
+    }
 }
