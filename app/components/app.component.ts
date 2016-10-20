2df4e4567670bbb67000d9efe4df101c38c2799e
Alison Moura <alison.oghino@gmail.com>
2016-10-20 12:27:24 -0300

    Taking de HEROES const from app.component.ts

--- app/components/app.component.ts
+++ app/components/app.component.ts
@@ -2,19 +2,6 @@ import { Component } from '@angular/core';
 import { HeroDetailComponent } from './hero-detail.component';
 import { Hero } from './../models/hero';
 
-const HEROES: Hero[] = [
-  { id: 11, name: 'Mr. Nice' },
-  { id: 12, name: 'Narco' },
-  { id: 13, name: 'Bombasto' },
-  { id: 14, name: 'Celeritas' },
-  { id: 15, name: 'Magneta' },
-  { id: 16, name: 'RubberMan' },
-  { id: 17, name: 'Dynama' },
-  { id: 18, name: 'Dr IQ' },
-  { id: 19, name: 'Magma' },
-  { id: 20, name: 'Tornado' }
-];
-
 @Component({
   selector: 'my-app',
   template: `
@@ -81,7 +68,7 @@ const HEROES: Hero[] = [
 })
 export class AppComponent {
   title = 'Tour of Heroes';
-  heroes = HEROES;
+  heroes = [];
   selectedHero: Hero;
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
