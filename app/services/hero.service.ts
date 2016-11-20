f3d5d676e031705dc53d6de0afcd8b515540923f
Alison Moura <alison.oghino@gmail.com>
2016-11-20 19:58:20 -0300

    Creating getHeroesSlowly in HeroService

--- app/services/hero.service.ts
+++ app/services/hero.service.ts
@@ -4,8 +4,12 @@ import { Hero } from './../models/hero';
 import { HEROES } from './../mocks/mock-heroes';
 
 @Injectable()
-export class HeroService{
-    getHeroes() : Promise<Hero[]>{
+export class HeroService {
+    getHeroes(): Promise<Hero[]> {
         return Promise.resolve(HEROES);
+    }
+    getHeroesSlowly(): Promise<Hero[]> {
+        return new Promise<Hero[]>(resolve => setTimeout(resolve, 2000))
+            .then(() => this.getHeroes());
     }
 }
