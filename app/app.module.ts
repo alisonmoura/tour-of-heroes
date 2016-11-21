6ae3784e637efe3f2d61595a8d0350c04baa9d28
Alison Moura <alison.oghino@gmail.com>
2016-11-20 21:12:39 -0300

    Separating heroes listing logic into HeroesComponent

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,13 +1,16 @@
-import { NgModule }      from '@angular/core';
+import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule }   from '@angular/forms';
-import { AppComponent }   from './components/app.component';
+import { FormsModule } from '@angular/forms';
+import { AppComponent } from './components/app.component';
+import { HeroesComponent } from './components/heroes.component';
 import { HeroDetailComponent } from './components/hero-detail.component'
+import { HeroService } from './services/hero.service'
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule],
-  declarations: [ AppComponent, HeroDetailComponent ],
-  bootstrap:    [ AppComponent ]
+  imports: [BrowserModule, FormsModule],
+  declarations: [AppComponent, HeroDetailComponent, HeroesComponent],
+  providers: [HeroService],
+  bootstrap: [AppComponent]
 })
 
 export class AppModule { }
