0107a8b73978b42dbae86a2834403db70e651278
Alison Moura <alison.oghino@gmail.com>
2016-11-21 09:31:06 -0300

    Getting the top 4 heroes

--- app/components/dashboard.component.ts
+++ app/components/dashboard.component.ts
@@ -1,8 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+import { HeroService } from './../services/hero.service';
+import { Hero } from './../models/hero';
 
 @Component({
-    template: '<h3>My Dashboard</h3>',
-    selector:'my-dashboard'
+    moduleId: module.id,
+    templateUrl: './../templates/dashboard.component.html',
+    selector: 'my-dashboard'
 })
-export class DashboardComponent{
+export class DashboardComponent implements OnInit {
+
+    heroes: Hero[] = [];
+
+    constructor(private heroService: HeroService) { }
+
+    ngOnInit(): void {
+        this.heroService.getHeroes()
+            .then(heroes => this.heroes = heroes.slice(1, 5));
+    }
+
 }
