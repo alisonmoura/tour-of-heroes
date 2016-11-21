f4b0079c9c0dc0a577b2c7af213e75426078ed45
Alison Moura <alison.oghino@gmail.com>
2016-11-21 09:10:44 -0300

    Creating dashboard link

--- app/components/app.component.ts
+++ app/components/app.component.ts
@@ -5,6 +5,7 @@ import { Component } from '@angular/core';
     template: `
         <h1>{{title}}</h1>
         <a routerLink="/heroes">Heroes</a>
+        <a routerLink="/dashboard">Dashboard</a>
         <router-outlet></router-outlet>
     `
 })
